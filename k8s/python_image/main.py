7efa221838efaa29ab1ed74e4888af5c2fd90abc
OES52 <choehanbeen@gmail.com>
2023-12-04 02:43:39 +0000

    add comment

--- k8s/python_image/main.py
+++ k8s/python_image/main.py
@@ -4,27 +4,36 @@ import sys
 import json                                     
 import os                                       
                                                 
-arg = os.environ.get('ARGS')                    
-answer = os.environ.get('ANSWER')               
-json_arg = json.loads(arg.replace("'","\""))    
-                                                
-captured_output = io.StringIO()                 
-sys.stdout = captured_output                    
-                                                
-return_output = main(*json_arg)                 
-                                                
-sys.stdout = sys.__stdout__                     
-print_output = captured_output.getvalue()       
-                                                
-correct = str(return_output)==answer            
-                                                
-data = {                                        
-    "Print" : print_output,                     
-    "Return" : str(return_output),              
-    "Solution" : answer,                        
-    "Win" : correct                             
-}                                               
-                                                
-json_string = json.dumps(data)                  
-                                                
-print(json_string)                              
+#환경변수 로드
+arg = os.environ.get('ARGS')
+answer = os.environ.get('ANSWER')
+json_arg = json.loads(arg.replace("'","\""))
+
+#출력 캐치
+captured_output = io.StringIO()
+sys.stdout = captured_output
+
+#함수 불러와서 실행
+try :
+    return_output = main(*json_arg)
+except Exception as e :
+    return_output = e
+
+#돌려놓기 (필요없나?)
+sys.stdout = sys.__stdout__
+print_output = captured_output.getvalue()
+
+#채점
+correct = str(return_output)==answer
+
+#json 패키징
+data = {
+    "Print" : print_output,
+    "Return" : str(return_output),
+    "Solution" : answer,
+    "Win" : correct
+}
+
+json_string = json.dumps(data)
+
+print(json_string)                        
