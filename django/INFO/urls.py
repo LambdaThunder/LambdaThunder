7ee12df2be901dab9de8018279bf90a6be49f77d
cjy <zoopooh779@gmail.com>
2023-12-08 08:08:38 +0000

    list_pagination

--- django/INFO/urls.py
+++ django/INFO/urls.py
@@ -10,4 +10,6 @@ urlpatterns = [
     path('login_view/', views.login_view, name="login_view"),
     path('list2/', views.list2, name="list2"),
     path('logout/', views.logout_view, name='logout'),
+    path('quiz/', views.quiz, name='quiz'), # quiz 함수 선언, quiz 로 호출하고 quiz 로 내보냄
+    path('index', views.get_parameter, name='parameter'), # get_parameter 함수 선언, parameter 로 호출하고 index 로 내보냄
 ]
